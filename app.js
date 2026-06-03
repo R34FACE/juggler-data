378a14b3bf0ca98413e9ad5466f507416c31f9dc
R34FACE <ikimaster34@gmail.com>
2026-06-03 19:14:16 +0900

    Replace PWA PNG icons with SVG

--- app.js
+++ app.js
@@ -4284,3 +4284,9 @@ function importMastersCsv(file) {
     renderAll();
   });
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js").catch(console.error);
+  });
+}
